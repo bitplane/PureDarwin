9f410caed3e40e66282464d4122a23ae717f8855
Gareth Davidson <gaz@bitplane.net>
2026-08-11 15:51:52 +0100

    Pass external kernel inputs through XNU make

--- src/Kernel/xnu/preprocess_files.cmake
+++ src/Kernel/xnu/preprocess_files.cmake
@@ -5,26 +5,6 @@ if(XNU_EXTERNAL_SOURCE)
     configure_file(${XNU_SRC}/makedefs/MakeInc.cmd ${XNU_OBJ}/makedefs/MakeInc.cmd COPYONLY)
 
     file(READ ${XNU_SRC}/makedefs/MakeInc.def make_inc_def)
-    string(REPLACE
-        "LD\t= $(KC++) -nostdlib"
-        "LD\t= $(KC++) -nostdlib -fuse-ld=${LD_PATH}"
-        make_inc_def "${make_inc_def}")
-    string(REPLACE
-        "LDFLAGS_KERNEL_SDK\t= -L$(SDKROOT)/usr/local/lib/kernel -lfirehose_kernel"
-        "LDFLAGS_KERNEL_SDK\t= -L${FIREHOSE_KERNEL_LIBRARY_DIR} -lfirehose_kernel"
-        make_inc_def "${make_inc_def}")
-    string(REPLACE
-        "LD_KERNEL_LIBS\t= -lcc_kext"
-        "LD_KERNEL_LIBS\t= ${PROFILE_RUNTIME_LIBRARY_PATH}"
-        make_inc_def "${make_inc_def}")
-    string(REPLACE
-        "INCFLAGS_EXTERN\t= -I$(SRCROOT)/EXTERNAL_HEADERS"
-        "INCFLAGS_EXTERN\t= -I$(SRCROOT)/EXTERNAL_HEADERS -I${PTHREAD_HEADER_PATH}"
-        make_inc_def "${make_inc_def}")
-    string(REPLACE
-        "INCFLAGS_SDK\t= -I$(SDKROOT)/usr/local/include/kernel"
-        "INCFLAGS_SDK\t= -I${FIREHOSE_KERNEL_HEADER_PATH}"
-        make_inc_def "${make_inc_def}")
     file(WRITE ${XNU_OBJ}/makedefs/MakeInc.def "${make_inc_def}")
 
     file(READ ${XNU_SRC}/bsd/sys/make_symbol_aliasing.sh make_symbol_aliasing)
