6e242d4f4b07da5680cd14b48f97c70d2b514b0e
Gareth Davidson <gaz@bitplane.net>
2026-08-12 08:31:20 +0100

    Build libdispatch with current Clang

--- scripts/prepare-external-sources.sh
+++ scripts/prepare-external-sources.sh
@@ -13,6 +13,8 @@ ASL_PATCH=$SCRIPT_DIR/../patches/libplatform-161.20.1/0001-return-an-error-when-
 LIBC_DIRSTAT_SOURCE=$SOURCE_ROOT/libc/libdarwin/dirstat.c
 LIBC_DIRSTAT_PATCH=$SCRIPT_DIR/../patches/libc-1244.30.3/0001-build-dirstat-without-private-apfs-headers.patch
 LIBC_VARIANT_PATCH=$SCRIPT_DIR/../patches/libc-1244.30.3/0002-build-os-variant-without-private-xpc.patch
+LIBDISPATCH_INTERNAL=$SOURCE_ROOT/libdispatch/src/internal.h
+LIBDISPATCH_ASSERT_PATCH=$SCRIPT_DIR/../patches/libdispatch-913.30.4/0001-avoid-typeof-on-bit-fields.patch
 
 if grep -q 'if (fd == -1) return;' "$ASL_SOURCE"; then
     patch -d "$SOURCE_ROOT/libplatform" -p1 < "$ASL_PATCH"
@@ -26,3 +28,8 @@ grep -q '__has_include(<apfs/apfs_fsctl.h>)' "$LIBC_DIRSTAT_SOURCE"
 grep -q '__has_include(<xpc/xpc.h>)' "$SOURCE_ROOT/libc/libdarwin/variant.c" || {
     patch -d "$SOURCE_ROOT/libc" -p1 < "$LIBC_VARIANT_PATCH"
 }
+
+if grep -q 'typeof(e) _e = (e);' "$LIBDISPATCH_INTERNAL"; then
+    patch -d "$SOURCE_ROOT/libdispatch" -p1 < "$LIBDISPATCH_ASSERT_PATCH"
+fi
+grep -q 'long _e = (long)(e);' "$LIBDISPATCH_INTERNAL"
