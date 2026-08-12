3a7e11b34a0faf983023a335e08fc2af7f6f7f95
Gareth Davidson <gaz@bitplane.net>
2026-08-12 11:16:33 +0100

    Complete the ASL system runtime dependencies

--- src/Libraries/libSystem/libc/compat/flsl.c
+++ src/Libraries/libSystem/libc/compat/flsl.c
@@ -1,6 +1,14 @@
 #include <limits.h>
 
 int
+fls(int value)
+{
+	if (value == 0)
+		return 0;
+	return (int)(sizeof(value) * CHAR_BIT - __builtin_clz((unsigned int)value));
+}
+
+int
 flsl(long value)
 {
 	if (value == 0)
