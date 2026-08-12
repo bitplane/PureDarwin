83d77efa1b6809912fd2d71e267bf5115653122a
Gareth Davidson <gaz@bitplane.net>
2026-08-12 10:57:53 +0100

    Complete the dyld libc++ failure runtime

--- src/Libraries/dyld/compat/runtime.cpp
+++ src/Libraries/dyld/compat/runtime.cpp
@@ -21,3 +21,19 @@ extern "C" [[noreturn]] void libcxx_string_length_error()
 {
     dyld::halt("libc++ string length error");
 }
+
+extern "C" [[noreturn]] void libcxx_string_out_of_range()
+    __asm("__ZNKSt3__121__basic_string_commonILb1EE20__throw_out_of_rangeEv");
+
+extern "C" [[noreturn]] void libcxx_string_out_of_range()
+{
+    dyld::halt("libc++ string out of range");
+}
+
+extern "C" [[noreturn]] void libcxx_vector_length_error()
+    __asm("__ZNKSt3__120__vector_base_commonILb1EE20__throw_length_errorEv");
+
+extern "C" [[noreturn]] void libcxx_vector_length_error()
+{
+    dyld::halt("libc++ vector length error");
+}
