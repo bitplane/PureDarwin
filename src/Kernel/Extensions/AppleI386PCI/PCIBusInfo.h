ace48db158af2ce28235f139964f82ef3d65a47f
Gareth Davidson <gaz@bitplane.net>
2026-08-12 15:42:51 +0100

    Provide the legacy PCI boot ABI

---
+++ src/Kernel/Extensions/AppleI386PCI/PCIBusInfo.h
@@ -0,0 +1,21 @@
+#ifndef PUREDARWIN_PCI_BUS_INFO_H
+#define PUREDARWIN_PCI_BUS_INFO_H
+
+typedef struct {
+    union {
+        struct {
+            unsigned char configMethod1 : 1;
+            unsigned char configMethod2 : 1;
+            unsigned char : 2;
+            unsigned char specialCycle1 : 1;
+            unsigned char specialCycle2 : 1;
+        } s;
+        unsigned char d;
+    } u_bus;
+    unsigned char maxBusNum;
+    unsigned char majorVersion;
+    unsigned char minorVersion;
+    unsigned char BIOSPresent;
+} PCI_bus_info_t;
+
+#endif
